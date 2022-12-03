1e0f8b75f8d8a9e78ea11931e86b045ad1c3b3e8
Marcel Körtgen <marcel.koertgen@gmail.com>
2022-12-03 17:38:11 +0100

    feat(hello.babylonjs): initial commit

---
+++ hello.babylonjs/README.md
@@ -0,0 +1,18 @@
+# hello.babylonjs
+
+Examples using [Babylon.js](https://github.com/BabylonJS/Babylon.js)
+
+## Getting Started
+
+```shell
+# Install dependencies
+npm install
+# Start development mode...
+npm run dev
+# ...or build for production
+npm run build
+```
+
+## Examples
+
+- [paganaye/babylonjs-vite-boilerplate](https://github.com/paganaye/babylonjs-vite-boilerplate) - BabylonJs + vite Boilerplate
---
+++ hello.babylonjs/server.js
@@ -0,0 +1,24 @@
+// NOTE: not really a server, just a script to run in node.js
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+const router = express.Router();
+const fs = require("fs");
+
+app.use(express.static(`${__dirname}/dist`));
+
+app.use("/", router);
+
+router.get("*", (req, res, next) => {
+  // uncomment the line below to see the file requests on the console
+
+  if (fs.existsSync(`${__dirname}` + req.url + ".gz")) {
+    res.set("Content-Encoding", "gzip");
+    res.set("Content-Type", "application/javascript");
+    req.url = req.url + ".gz";
+  }
+  res.sendFile(`${__dirname}` + req.url);
+});
+
+app.listen(port);
+console.log("App listening on port", port);
